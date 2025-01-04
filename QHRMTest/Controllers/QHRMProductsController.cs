79a5be68ad3a520e943d8da07b7fe14ceecb160a
chandangupta <chandangupta@alphafinsoft.net.in>
2025-01-04 12:20:25 +0530

    initial commit

---
+++ QHRMTest/Controllers/QHRMProductsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QHRMTest.Data;
+using QHRMTest.Models;
+
+namespace QHRMTest.Controllers
+{
+    public class QHRMProductsController : Controller
+    {
+        private readonly QHRMProductRepository _repository;
+
+        public QHRMProductsController(IConfiguration configuration)
+        {
+            _repository = new QHRMProductRepository(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public IActionResult Index()
+        {
+            var products = _repository.GetAllProducts();
+            return View(products);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(QHRMProducts product)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.AddProduct(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = _repository.GetProductById(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(QHRMProducts product)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.UpdateProduct(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        public IActionResult Details(int id)
+        {
+            var product = _repository.GetProductById(id);
+            return View(product);
+        }
+            public IActionResult Delete(int id)
+                
+        {
+            var product = _repository.GetProductById(id);
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _repository.DeleteProduct(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ QHRMTest/Data/QHRMProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using QHRMTest.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QHRMTest.Data
+{
+    public class QHRMProductRepository
+    {
+        private readonly string _connectionString;
+
+        public QHRMProductRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<QHRMProducts> GetAllProducts()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            return connection.Query<QHRMProducts>("SELECT * FROM QHRMProducts").ToList();
+        }
+
+        public void AddProduct(QHRMProducts product)
+        {
+            product.Created = DateTime.Now;
+            using var connection = new SqlConnection(_connectionString);
+            const string sql = "INSERT INTO QHRMProducts (Name, Description, Price, Created) VALUES (@Name, @Description, @Price, @Created)";
+            connection.Execute(sql, product);
+        }
+
+
+        public QHRMProducts GetProductById(int id)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            return connection.QuerySingleOrDefault<QHRMProducts>("SELECT * FROM QHRMProducts WHERE Id = @Id", new { Id = id });
+        }
+
+        public void UpdateProduct(QHRMProducts product)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            const string sql = "UPDATE QHRMProducts SET Name = @Name, Description = @Description, Price = @Price WHERE Id = @Id";
+            connection.Execute(sql, product);
+        }
+
+
+        public void DeleteProduct(int id)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            connection.Execute("DELETE FROM QHRMProducts WHERE Id = @Id", new { Id = id });
+        }
+    }
+}
